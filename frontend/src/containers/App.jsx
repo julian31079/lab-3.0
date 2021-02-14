4bb73b663aa96e9779406ebb8362ed34739184c5
David Polania <david.polania3@gmail.com>
2021-02-14 04:50:04 -0500

    EsLint

--- frontend/src/containers/App.jsx
+++ frontend/src/containers/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import Header from '../components/Header'
-import Login from '../components/Login'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import Header from "../components/Header";
+import Login from "../components/Login";
+
 const App = () => (
-    <div className="App">
-        <Login/>
-    </div>
-)
+  <div className="App">
+    <Router>
+      <Header />
+    <div>
+        <Switch>
+          {
+            <Route path="/" exact="true" component={Login} />
+          }
+        </Switch>
+      </div>
+      
+    </Router>
+  </div>
+);
 
 export default App;
